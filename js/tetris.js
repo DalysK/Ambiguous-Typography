c3e6230c1d3365292a86ebf4d84b9fde7868c1e7
DalysK <heangsodalyskrin@gmail.com>
2025-04-20 09:10:27 -0400

    Update tetris.js

--- js/tetris.js
+++ js/tetris.js
@@ -1,1 +1,11 @@
+const grid = document.getElementById("grid-container-tetris");
+
+const COLS = 10;
+const ROWS = 20;
+
+for (let i = 0; i < COLS * ROWS; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("grid-cell");
+  grid.appendChild(cell);
+}
 
